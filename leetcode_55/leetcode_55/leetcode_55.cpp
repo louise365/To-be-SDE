6ff68bde577f1aa56fb851b5aec8ace3b8e7c56b
louise365 <louise_290@foxmail.com>
2020-02-04 16:58:34 +0800

    leetcode_55

--- leetcode_55/leetcode_55/leetcode_55.cpp
+++ leetcode_55/leetcode_55/leetcode_55.cpp
@@ -4,19 +4,51 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
+class Solution1 {
+public:
+	bool canJump(vector<int>& nums) {
+		return DFS(nums,0);
+	}
+	bool DFS(vector<int>& nums, unsigned int idx) {
+		if (nums[idx] >= 0)
+		{
+			if (nums.size() - 1 <= idx + nums[idx])
+				return  true;
+			else
+			{
+				for (unsigned int i = idx+1; i <= idx + nums[idx]; i++)
+					if (DFS(nums,i))
+						return  true;
+				return  false;
+			}
+		}
+		else
+			return false;
+	}
+};
 class Solution {
 public:
 	bool canJump(vector<int>& nums) {
-
+		int _max = 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (i > _max)	return false;
+			_max = max(_max, i + nums[i]);
+			if (_max >= nums.size()-1)
+				return true;
+		}
+		return false;
 	}
 };
-
 int main()
 {
 	Solution s;
-	vector<int> vt;
+	vector<int> vt = { 2,3,1,1,4 };
+	vector<int> vt1 = { 3,2,1,0,4 };
 	cout << s.canJump(vt) << endl;
+	cout << s.canJump(vt1) << endl;
     return 0;
 }
 
