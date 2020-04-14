cbdb12c5c69f720b5ddebf2636af9617a2452014
louise365 <louise_290@foxmail.com>
2020-04-14 15:18:36 +0800

    Apr14

--- leetcode_58/leetcode_58.cpp
+++ leetcode_58/leetcode_58.cpp
@@ -1,38 +1,34 @@
-﻿// leetcode_38.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// leetcode_58.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Solution {
 public:
-    string countAndSay(int n) {
-        string s = "1";
-        for (int i = 1; i < n; ++i) {
-            string t;
-            int count = 1;
-            char c = s[0];
-            for (int j = 1; j < s.size(); ++j) {
-                if (s[j] != c) {
-                    t += to_string(count) + c;
-                    c = s[j];
-                    count = 1;
-                }
-                else
-                    ++count;
-            }
-            if(count)
-                t += to_string(count) + c;
-            s = t;
+    int lengthOfLastWord(string s) {
+        int idx = s.size() - 1;
+        while (idx >= 0) {
+            if (s[idx] == ' ')
+                --idx;
+            else
+                break;
         }
-        return s;
+        int idx2 = idx;
+        while (idx2 >= 0) {
+            if (s[idx2] != ' ')
+                --idx2;
+            else
+                break;
+        }
+        return idx - idx2;
     }
 };
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution s;
+    cout << s.lengthOfLastWord("a a ") << endl;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
